07d8d2ded0c57bbb650f5b22bb1ee3264ea23138
sedwards154 <edwardsseji@gmail.com>
2019-07-18 17:09:27 -0500

    finished

--- main.py
+++ main.py
@@ -1,211 +1,108 @@
 from flask import Flask, request
 
-
-
 import cgi
 
-
-
 import os
-
-
 
 import jinja2
 
 
 
-
-
-
-
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-
-
 
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 
 
-
-
-
-
 app = Flask(__name__)
-
-
 
 app.config['DEBUG'] = True
 
 
 
-
-
-
-
 @app.route("/")
-
-
 
 def index():
 
-
-
 	template = jinja_env.get_template('index.html')
-
-
 
 	return template.render(password1='')
 
 
 
-
-
-
-
 	
-
-
 
 @app.route("/create", methods=["POST"])
 
-
-
 def create():
-
-
 
 	verified = False
 
-
-
 	var1 = 0
-
-
 
 	var2 = 0
 
-
-
 	template = jinja_env.get_template('index.html')
-
-
 
 	welcome = jinja_env.get_template('welcome.html')
 
-
-
 	username = request.form["username"]
-
-
 
 	password1 = request.form["pas1"]
 
-
-
 	password2 = request.form["pas2"]
-
-
 
 	email = request.form["email"]
 
-
-
 	if len(email) > 0:
-
-
 
 		for char in email:
 
-
-
 			if char=="@":
-
-
 
 				var1+=1
 
-
-
 			if char==".":
-
-
 
 				var2+=1
 
-
-
 			if var1==1 and var2 >=1:
-
-
 
 				verified = True
 
-
-
 			else:
-
-
 
 				verified = False
 
-
-
 	else:
-
-
 
 		verified = True
 
-
-
 	if verified == False:
-
-
 
 		return template.render(email1=" This is not a valid email.")
 
-
-
 	elif verified == True:
-
-
 
 		if password1 != password2:
 
-
-
-			return template.render(password2=" Passwords do not match!")
-
-
+			return template.render(pass2=" Passwords do not match!")
 
 		elif password1 == password2:
 
-
-
 			if len(password1) >= 3 and len(password1) <=20:
-
-
 
 				if len(username) > 0:
 
-
-
 					return welcome.render(user=username)
-
-
 
 				else:
 
-
-
 					return template.render(name1=" Must have a username")
-
-
 
 			else:
 
+				return template.render(pass2=" Password must be between 3 and 20 characters long.")
 
 
-				return template.render(password2=" Password must be between 3 and 20 characters long.")
-
-app.run ()
+				app.run ()
